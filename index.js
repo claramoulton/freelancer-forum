c95b27a3dfd6a04fe70ff3ae18d4eece8b7e75bb
Clara Moulton <claramoulton@Claras-MBP.lan>
2023-12-01 10:52:42 -0700

    last commit

--- index.js
+++ index.js
@@ -7,23 +7,49 @@ const freelancers = [
     {name: "Dr. Pressure", price: 51, occupation: "programmer"}
 ];
 
-spanCount.innerHTML = freelancers.length;
+const names = ["Dr. Slice", "Dr. Pressure", "Prof. Possibility", "Prof. Prism", "Dr. Impulse", "Prof. Spark", "Dr. Wire", "Prof. Goose"];
 
-let sum = 0;
-freelancers.forEach((freelancer)=> {
-    sum = sum + freelancer.price;
-});
-const average = sum / freelancers.length;
-spanAverageRate.innerHTML = average;
+const occupations = ["gardener", "programmer", "teacher", "gardner"];
 
-const html = freelancers.map((freelancer)=> {
-    return `
-        <li>
-        ${freelancer.name} <br>
-        price: ${freelancer.price} <br>
-        occupation: ${freelancer.occupation} <br>
-        </li>
-    `;
-}).join('');
+function render(){
+    spanCount.innerHTML = freelancers.length;
 
-ul.innerHTML = html;
+    let sum = 0;
+    freelancers.forEach((freelancer)=> {
+        sum = sum + freelancer.price;
+    });
+    const average = sum / freelancers.length;
+    spanAverageRate.innerHTML = average.toFixed(2);
+
+    const html = freelancers.map((freelancer)=> {
+        return `
+          <li>
+             ${freelancer.name} <br>
+             price: $${freelancer.price.toFixed(2)} <br>
+             occupation: ${freelancer.occupation} <br>
+         </li>
+     `;
+    }).join('');
+
+    ul.innerHTML = html;
+}
+
+render();
+
+const interval = setInterval(()=> {
+    const nameIndex = Math.floor(Math.random()*names.length); 
+    const name = names[nameIndex];
+    const price = Math.ceil(Math.random()*100);
+    const occupationIndex = Math.floor(Math.random()*occupations.length); 
+    const occupation = occupations[occupationIndex];
+    const randomFreelancer = {
+        name: name,
+        price: price,
+        occupation: occupation
+    };
+    freelancers.push(randomFreelancer);
+    render();
+    if(freelancers.length === 20){
+    clearInterval(interval);
+    }
+}, 1000)
